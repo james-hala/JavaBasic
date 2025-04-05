afe047679449428d468c09c0b54a20318a81c4f2
james-hala <halajameshala@gmail.com>
2025-04-06 03:12:18 +0800

    Create Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,25 @@
+/*
+javac -version(檢查編譯器compiler)
+java -version(檢查執行run版本)
+dir/w(列出清單)
+cd ..(資料夾回上一層)
+cd\(資料夾回最上層)
+cd 資料夾名稱(切換資料夾)
+cls(清除)
+*/
+/*
+開發步驟
+撰寫原始碼source-->檔名.java
+送到編譯器compiler-->javac 檔名.java
+檢查語法,產生機器碼-->檔名.class
+測試執行-->java 檔名
+修正錯誤,抓bug
+*/
+class Ex1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello java");
+		//要跑出字要打javac -encoding utf-8 檔名.java	
+	}
+}

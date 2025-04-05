5bdd8d4631fef92a52e3db8fb4c17305694f7581
james-hala <halajameshala@gmail.com>
2025-04-06 03:12:03 +0800

    Create Ex7.java

---
+++ Ex7.java
@@ -0,0 +1,22 @@
+/*
+關係運算子relational operator
+1.boolean-->合-->true
+		  -->不合-->false
+2.>,<,>=,<=,==(等於),!=(不等於)
+*/
+class Ex7
+{
+	public static void main(String[] args) 
+	{
+		int chinese=80;
+		int english=60;
+		System.out.println("chinese="+chinese);
+		System.out.println("english="+english);
+		System.out.println("chinese > english--->"+(chinese>english));
+		System.out.println("chinese < english--->"+(chinese<english));
+		System.out.println("chinese >= english-->"+(chinese>=english));
+		System.out.println("chinese <= english-->"+(chinese<=english));
+		System.out.println("chinese == english-->"+(chinese==english));
+		System.out.println("chinese != english-->"+(chinese!=english));
+	}
+}
